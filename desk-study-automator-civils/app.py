5ae43b845e122088fae7aa90686543743a3de95c
ElliottWoodEngineers <digital@elliottwood.co.uk>
2026-06-23 16:07:37 +0100

    create webview to show html file

--- desk-study-automator-civils/app.py
+++ desk-study-automator-civils/app.py
@@ -1,9 +1,15 @@
 import viktor as vkt
+from pathlib import Path
 
 
 class Parametrization(vkt.Parametrization):
-    pass # Welcome to VIKTOR! You can add your input fields here. Happy Coding!
+    pass
 
 
 class Controller(vkt.Controller):
     parametrization = Parametrization
+
+    @vkt.WebView('FRA Generator')
+    def get_web_view(self, params, **kwargs):
+        html_path = Path(__file__).parent / 'files' / 'FRA_Generator.html'
+        return vkt.WebResult.from_path(html_path)
